6e9bd44864ac2be028f7b0083c9ffe212bde4348
Vasili Vasilyeu <vasili_vasilyeu@epam.com>
2024-07-21 18:06:32 -0700

    Add a "help" command

--- main.c
+++ main.c
@@ -8,6 +8,13 @@
 
 #define INPUT_BUFFER_SIZE 16
 
+static int help(void);
+
+struct pico_con_command commands[] = {
+	{"help", help},
+	{NULL, NULL},
+};
+
 int main()
 {
 	bi_decl(bi_program_description(PROG_DESC));
@@ -24,7 +31,7 @@ int main()
 
 	printf("\n" PROG_DESC "\n");
 
-	if (pico_con_loop(INPUT_BUFFER_SIZE) == 0)
+	if (pico_con_loop(commands, INPUT_BUFFER_SIZE) == 0)
 	{
 		printf("Serial console closed.\n");
 	}
@@ -37,4 +44,11 @@ int main()
 	{
 		tight_loop_contents();
 	}
+}
+
+int help(void)
+{
+	printf("Available commands:\n\n" \
+	       "\thelp - prints this message.\n\n");
+	return PICO_CON_COMMAND_SUCCESS;
 }
